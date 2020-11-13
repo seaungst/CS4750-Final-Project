e3e996d447872ed4e34a0f6b5b9c9d85634bd73a
seaungst <selena.elise03@gmail.com>
2020-11-12 22:50:20 -0800

    Added signup and login pages from homepage

--- app/src/main/java/com/bignerdranch/android/cs4750finalproject/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/cs4750finalproject/MainActivity.kt
@@ -2,10 +2,71 @@ package com.bignerdranch.android.cs4750finalproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.login.*
+import kotlinx.android.synthetic.main.sign_up.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var handler:DatabaseHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        handler = DatabaseHelper(this)
+
+        showHomepage()
+
+        //Shows sign up page when sign up button is clicked
+        sign_up.setOnClickListener {
+            showSignUp()
+        }
+
+        //Shows login page when login button is clicked
+        login.setOnClickListener {
+            showLogin()
+        }
+
+        //Attempts to login if login button on login page is clicked
+        login_button.setOnClickListener {
+
+            //Prints login success message if true
+            if (handler.verifyAccount(login_username.text.toString(), login_password.text.toString())) {
+                Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+            }
+
+            //Prints incorrect attempt message if false
+            else {
+                Toast.makeText(this, "Username or password is incorrect.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        //Attempts to save user info if save button on sign up page is clicked
+        save.setOnClickListener {
+            handler.insertUserData(username.text.toString(), password.text.toString())
+            showHomepage()
+        }
     }
+
+    private fun showSignUp() {
+        sign_up_layout.visibility = View.VISIBLE
+        login_layout.visibility = View.GONE
+        homepage.visibility = View.GONE
+    }
+
+    private fun showLogin() {
+        sign_up_layout.visibility = View.GONE
+        login_layout.visibility = View.VISIBLE
+        homepage.visibility = View.GONE
+    }
+
+    private fun showHomepage() {
+        sign_up_layout.visibility = View.GONE
+        login_layout.visibility = View.GONE
+        homepage.visibility = View.VISIBLE
+    }
+
 }
