5141f965ede43a7eedc1382f0ff9235e2d6fcc6b
seaungst <selena.elise03@gmail.com>
2020-11-12 19:42:47 -0800

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/cs4750finalproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.cs4750finalproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
