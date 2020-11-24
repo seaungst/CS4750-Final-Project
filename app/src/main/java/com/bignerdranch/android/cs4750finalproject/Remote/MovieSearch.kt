389881dc690fae7a40abd4a55f358d6e469c6577
Sonia Carrillo <soniasofiacarrillo@gmail.com>
2020-11-23 19:41:44 -0800

    Added movieSearch layout page and kotlin file

---
+++ app/src/main/java/com/bignerdranch/android/cs4750finalproject/Remote/MovieSearch.kt
@@ -0,0 +1,8 @@
+package com.bignerdranch.android.cs4750finalproject.Remote
+
+class MovieSearch {
+    //hold the imdb posters by importin database garb
+    //include the home button to return
+    //include webview onClick
+
+}
