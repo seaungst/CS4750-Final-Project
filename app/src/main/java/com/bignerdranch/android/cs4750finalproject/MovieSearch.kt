49b38bc3f13df49207932518af48b9826b56df45
Sonia Carrillo <soniasofiacarrillo@gmail.com>
2020-11-28 15:18:59 -0800

    Added INTERNET permission, movie fragment and recyclerviews

--- app/src/main/java/com/bignerdranch/android/cs4750finalproject/MovieSearch.kt
+++
@@ -1,8 +0,0 @@
-package com.bignerdranch.android.cs4750finalproject
-
-class MovieSearch {
-    //hold the imdb posters by importin database garb
-    //include the home button to return
-    //include webview onClick
-//testy testy
-}
