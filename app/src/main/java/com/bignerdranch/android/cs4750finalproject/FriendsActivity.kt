0f685992c82f33b181ff5406499d1e25a876c9d4
Camryn Sumabat <51983267+camrynsumabat@users.noreply.github.com>
2020-12-01 17:42:30 -0800

    Added search bar in friends page!! Cleaned up my other code

--- app/src/main/java/com/bignerdranch/android/cs4750finalproject/FriendsActivity.kt
+++ app/src/main/java/com/bignerdranch/android/cs4750finalproject/FriendsActivity.kt
@@ -2,6 +2,7 @@ package com.bignerdranch.android.cs4750finalproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_friends.*
 import kotlinx.android.synthetic.main.activity_watch_list.*
@@ -20,6 +21,12 @@ class FriendsActivity : AppCompatActivity() {
         friends_rv_recyclerView.adapter = FriendsRecyclerAdapter(friendsTitleList)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.friend_search_bar, menu)
+        return true
+    }
+
     private fun addToList(friends_title: String){
         friendsTitleList.add(friends_title)
     }
@@ -29,16 +36,4 @@ class FriendsActivity : AppCompatActivity() {
             addToList("Friend # $i")
         }
     }
-    /*override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_friends)
-
-        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
-
-        if (currentFragment == null) {
-            val fragment = FriendsListFragment.newInstance()
-            supportFragmentManager.beginTransaction().add(R.id.fragment_container, fragment)
-                .commit()
-        }
-    }*/
 }
