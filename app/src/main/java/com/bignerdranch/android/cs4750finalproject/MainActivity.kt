a5ea0cb535814db8eb4e1e00b6d4f5f8459a36bf
seaungst <selena.elise03@gmail.com>
2020-12-02 20:58:37 -0800

    updated main activity

--- app/src/main/java/com/bignerdranch/android/cs4750finalproject/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/cs4750finalproject/MainActivity.kt
@@ -4,17 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var auth: FirebaseAuth
-
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        auth = FirebaseAuth.getInstance()
 
         //Shows sign up page when sign up button is clicked
         sign_up_homepage.setOnClickListener {
